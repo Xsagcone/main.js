1868abcaa42ed1f8a68fb8fb0f488f0af6f74b22
Xsagcone <saul.agc123@gmail.com>
2019-10-10 13:16:38 -0500

    comind.js

---
+++ Documents/sagc/maind/maind.js
@@ -0,0 +1,16 @@
+let cantidadDeDolares = 500
+
+let tasaDeDolares =19
+
+let cantidadDePesossAAhorrar = cantidadDeDolares * tasaDeDolares
+
+console.log(cantidadDePesossAAhorrar)
+
+
+
+let horasDelDia = 24
+let horaDeDormir = 11
+let HorarDeDespertar = 7
+
+let horasDormidas = horasDelDia - horaDeDormir - HorarDeDespertar
+ console.log(horasDormidas)
